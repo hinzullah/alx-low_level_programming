10eebab7f64944bacf44cac60410af28d4c76341
hinzullah <oduolakamol@gmail.com>
2022-10-16 02:57:04 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints alpha in upper and lower case
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
+
