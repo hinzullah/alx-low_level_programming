def840a2d974d7c26084695b561d739287cdf90b
hinzullah <oduolakamol@gmail.com>
2022-10-19 04:59:59 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 
 /**
  * _abs - function that computes the absolut value of an integer
- * 
+ *
  * @n: takes in integer type input for function
  *
  * Return: Always 0 (Success)
@@ -11,5 +11,5 @@ int _abs(int n)
 {
 	if (n < 0)
 		n = (-1) * n;
-	return n;
+	return (n);
 }
